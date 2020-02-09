6691f212947c60dd889b09f427c586fd43c49671
b123400 <i@b123400.net>
2020-02-09 22:26:02 +0900

    Basic parsing for Cheung Chau

---
+++ src/Scraping/Islands/CheungChau.hs
@@ -0,0 +1,124 @@
+module Scraping.Islands.CheungChau
+( timetables, route
+) where
+
+import Text.XML.Cursor (Cursor, attributeIs, child, following,
+                        ($.//), ($//), (>=>))
+import Data.Text (Text, pack, unpack, isInfixOf)
+import Text.Regex.TDFA ((=~))
+import Timetable hiding (timetables)
+import Scraping.Utility
+import Data.Maybe
+import Data.Time.LocalTime (TimeOfDay(..))
+import Debug.Trace
+
+timetables :: Cursor -> [Timetable TimeOfDay]
+timetables cursor = do
+    c <- findCheungChau cursor
+    ct <- findTimetableCursors c
+    cursorToTimetables ct
+
+
+route :: Cursor -> Route TimeOfDay
+route cursor = Route CheungChau $ timetables cursor
+
+findCheungChau :: Cursor -> [Cursor]
+findCheungChau cursor = cursor $.// (makeElement "a") >=> attributeIs (makeName "name") (Data.Text.pack "o01")
+
+findTimetableCursors :: Cursor -> [Cursor]
+findTimetableCursors =
+    findTableElements . nthMatch 5 (matchName "table") . following
+
+findTableElements :: Cursor -> [Cursor]
+findTableElements c = c $// (makeElement "table")
+
+cursorToTimetables :: Cursor -> [Timetable TimeOfDay]
+cursorToTimetables timeTable =
+    catMaybes $
+    [Weekday, Saturday, Holiday] >>= \day ->
+    [ToIsland, FromIsland]       >>= \direction ->
+    return $ findTimetable day direction timeTable
+
+findTimetable :: Day -> Direction -> Cursor -> Maybe (Timetable TimeOfDay)
+findTimetable day direction timeTable
+    | not $ hasDay timeTable day = Nothing
+    | otherwise =
+        let trs = timeTable $// (makeElement "tr")
+        in Just $ tableToTimetables day direction (flatContent <$> (child =<< getTDs =<< filter hasTwoTd trs))
+
+hasDay :: Cursor -> Day -> Bool
+hasDay cursor day =
+    case cursor $// (makeElement "td") of
+        (x:_) -> textHasDay day $ flatContent x
+        _ -> False
+    where
+        textHasDay :: Day -> Text -> Bool
+        textHasDay day text =
+            case day of
+                Weekday  -> isInfixOf (pack "Mondays") text
+                Saturday -> isInfixOf (pack "Saturdays") text
+                Holiday  -> isInfixOf (pack "Sundays") text
+
+textForDirection :: Direction -> Text
+textForDirection FromIsland = pack "From Cheung Chau"
+textForDirection ToIsland   = pack "From Central"
+
+tableToTimetables :: Day -> Direction -> [Text] -> Timetable TimeOfDay
+tableToTimetables day direction body =
+    Timetable { ferries   = catMaybes $ map (toFerry (isDay day)) $ findDirection (textForDirection direction) body
+              , day       = day
+              , direction = direction
+              }
+
+findDirection :: Text -> [Text] -> [Text]
+findDirection keyword list
+    | isInfixOf keyword (list !! 0) = filter notEmpty $ tail $ pickOdd list
+    | isInfixOf keyword (list !! 1) = filter notEmpty $ tail $ pickEven list
+    | otherwise                     = error ("not found")
+
+{-
+Match
+1.20 a.m.
+12.30 p.m.
+12.00 noon
+13.30 p.m.*
+14.30 p.m.*@
+15.30 p.m.#
+-}
+regexPattern :: String
+regexPattern = "([0-9]{1,2})\\.([0-9]{1,2}) ((a|p)\\.m\\.|noon)(\\*)?(@)?(#)?"
+
+splitCapture :: String -> [String]
+splitCapture timeString
+    | (length matches) == 1 = head matches
+    | otherwise             = error ("regex error " ++ timeString)
+    where matches = (cleanHTMLEntity timeString =~ regexPattern)
+
+toFerry :: ([String] -> Bool) -> Text -> Maybe (Ferry TimeOfDay)
+toFerry cond text =
+    let captures = splitCapture $ unpack text
+    in if cond captures
+       then Just (capturesToFerry captures)
+       else Nothing
+
+
+isDay :: Day -> [String] -> Bool
+isDay Holiday  _        = True
+isDay Weekday  captures = (captures !! 6) /= "@"
+isDay Saturday captures = (captures !! 7) /= "#"
+
+capturesToFerry :: [String] -> (Ferry TimeOfDay)
+capturesToFerry captures =
+    Ferry { time      = TimeOfDay { todHour = (if isAm then hours else hours + 12)
+                                  , todMin = minutes
+                                  , todSec = 0
+                                  }
+
+          , ferryType = if   isSlow
+                        then SlowFerry
+                        else FastFerry
+          }
+    where hours   = read (captures !! 1) `mod` 12
+          minutes = read (captures !! 2)
+          isAm    = (captures !! 3) == "a.m."
+          isSlow  = (captures !! 5) == "*"
