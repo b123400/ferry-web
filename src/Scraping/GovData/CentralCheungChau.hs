408b7101f9d859f2dd273356fc785243faf005d5
b123400 <i@b123400.net>
2020-05-28 20:52:38 +0900

    Separate csv logic

--- src/Scraping/GovData/CentralCheungChau.hs
+++ src/Scraping/GovData/CentralCheungChau.hs
@@ -2,24 +2,21 @@ module Scraping.GovData.CentralCheungChau
 ( fetch
 ) where
 
-import Control.Applicative ((<|>))
 import Control.Monad (mzero)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Cache (MonadCache, withCache)
-import Data.Bifunctor (first, second)
-import qualified Data.ByteString as B
+import Control.Newtype (Newtype)
 import Data.ByteString.Lazy (ByteString, fromStrict)
-import Data.Csv (decode, decodeByName, FromRecord(..), FromNamedRecord(..), FromField(..), HasHeader(..), (.:), (.!))
-import Data.Set (Set, singleton, isSubsetOf, empty, insert, intersection)
+import Data.Csv (FromRecord(..), FromNamedRecord(..), FromField(..), (.:), (.!))
+import Data.Set (Set, singleton, intersection)
 import Data.Text.Encoding (decodeUtf8)
 import Data.Time.Calendar (DayOfWeek(..))
 import Data.Time.Clock (NominalDiffTime)
-import GHC.Exts (toList)
 import Network.HTTP.Conduit (simpleHttp)
-import Text.XML.Cursor (Cursor, fromDocument)
 
 
 import Timetable hiding (timetables)
+import Scraping.GovData.Csv
 import Scraping.GovData.TimeString (parseTimeStr)
 
 csv :: (MonadIO m, MonadCache m ByteString) => m ByteString
@@ -30,34 +27,38 @@ csv = withCache "GovData-CentralCheungChau-CSV" $
 fetch :: (MonadIO m, MonadCache m ByteString, MonadCache m (Route NominalDiffTime)) => m (Route NominalDiffTime)
 fetch = withCache "GovData-CentralCheungChau" $ do
     res <- csv
-    timetables <- case parseCsv res of
+    timetables <- case parseCsv @Entry' @EnumDays' res of
         Left err -> error err
         Right a -> pure a
     pure $ Route CentralCheungChau timetables
 
-parseCsv :: ByteString -> Either String [Timetable NominalDiffTime]
-parseCsv bs = toTimetables <$> (parseWithHeader bs <|> parse bs)
+data EnumDays' = MonToFri | Sat | SunAndHoliday
 
-data EnumDays = MonToFri | Sat | SunAndHoliday
+instance EnumDays EnumDays' where
+    enumerated = [SunAndHoliday, Sat, MonToFri]
+    toDaySet SunAndHoliday = sunAndHoliday
+    toDaySet Sat = singleton $ Weekday Saturday
+    toDaySet MonToFri = weekdays
+
 
 newtype Direction' = Direction' Direction
-newtype Time' = Time' NominalDiffTime
 newtype Days' = Days' (Set Day) deriving (Show)
-newtype Remark' = Remark' (Entry -> (Set Day, Set Modifier))
+newtype Time' = Time' NominalDiffTime
+newtype Remark' = Remark' ((Set Day, Set Modifier) -> (Set Day, Set Modifier))
 
-data Entry = Entry
-    { _direction :: Direction'
-    , _days :: Days'
-    , _time :: Time'
-    , _remark :: Remark'
-    }
+type Entry' = Entry Direction' Days' Time' Remark'
 
-instance FromRecord Entry where
+instance Newtype Direction' Direction
+instance Newtype Days' (Set Day)
+instance Newtype Time' NominalDiffTime
+instance Newtype Remark' ((Set Day, Set Modifier) -> (Set Day, Set Modifier))
+
+instance FromRecord Entry' where
     parseRecord v
         | length v == 4 = Entry <$> v .! 0 <*> v .! 1 <*> v .! 2 <*> v .! 3
         | otherwise     = mzero
 
-instance FromNamedRecord Entry where
+instance FromNamedRecord Entry' where
     parseNamedRecord m = Entry <$> m .: "Direction"
                                <*> m .: "Service Date"
                                <*> m .: "Service Hour"
@@ -81,66 +82,20 @@ instance FromField Remark' where
     parseField "1" = pure $ Remark'
         $ addModifier SlowFerry
         . addModifier Freight
-        . emptyRemark
     -- 2 - denotes trip operated on Mondays to Fridays only (except public holidays)
     parseField "2" = pure $ Remark'
         $ addModifier FastFerry
         . modifyDays (intersection weekdays)
-        . emptyRemark
     -- 3 - denotes Ordinary ferry service and freight service is allowed and trip operated on Saturdays only (except public holidays)
     parseField "3" = pure $ Remark'
         $ addModifier SlowFerry
         . addModifier Freight
         . modifyDays (const $ singleton $ Weekday Saturday)
-        . emptyRemark
     -- 4 - denotes Ordinary ferry service and freight service is allowed and trip operated on Mondays to Fridays only (except public holidays)
     parseField "4" = pure $ Remark'
         $ addModifier SlowFerry
         . addModifier Freight
         . modifyDays (intersection weekdays)
-        . emptyRemark
     parseField "" = pure $ Remark'
         $ addModifier FastFerry
-        . emptyRemark
     parseField _ = fail "Cannot parse remark"
-
-
-emptyRemark :: Entry -> (Set Day, Set Modifier)
-emptyRemark (Entry _ (Days' d) _ _) = (d, empty)
-
-addModifier :: Modifier -> (Set Day, Set Modifier) -> (Set Day, Set Modifier)
-addModifier = second . insert
-
-modifyDays :: (Set Day -> Set Day) -> (Set Day, Set Modifier) -> (Set Day, Set Modifier)
-modifyDays = first
-
-toTimetables :: [Entry] -> [Timetable NominalDiffTime]
-toTimetables entries = do
-    days <- [SunAndHoliday, Sat, MonToFri]
-    direction <- [FromPrimary, ToPrimary]
-    let ferries = toFerry <$> findEntries direction days
-    [Timetable ferries (enumToDaySet days) direction]
-
-    where
-        remarkedEntries = patchDaySetByRemark <$> entries
-
-        findEntries direction days = filter (match direction days) remarkedEntries
-        match direction days (Entry (Direction' direction') (Days' days') _ _) = direction == direction' && (matchDays days days')
-        matchDays days days' = (enumToDaySet days) `isSubsetOf` days'
-
-        enumToDaySet SunAndHoliday = sunAndHoliday
-        enumToDaySet Sat = singleton $ Weekday Saturday
-        enumToDaySet MonToFri = weekdays
-
-toFerry :: Entry -> Ferry NominalDiffTime
-toFerry e@(Entry _ _ (Time' t) (Remark' r)) =
-    Ferry t (snd $ r e)
-
-patchDaySetByRemark :: Entry -> Entry
-patchDaySetByRemark e@(Entry direction' _ time' (Remark' r)) = Entry direction' (Days' $ fst $ r e) time' (Remark' r)
-
-parseWithHeader :: ByteString -> Either String [Entry]
-parseWithHeader bs = (toList . snd) <$> decodeByName bs
-
-parse :: ByteString -> Either String [Entry]
-parse bs = toList <$> decode HasHeader bs
---
+++ src/Scraping/GovData/Csv.hs
@@ -0,0 +1,83 @@
+module Scraping.GovData.Csv where
+
+import Control.Applicative ((<|>))
+import Control.Newtype (Newtype, pack, unpack)
+import Data.Bifunctor (first, second)
+import Data.ByteString.Lazy (ByteString)
+import Data.Csv (decode, decodeByName, FromRecord(..), FromNamedRecord(..), FromField(..), HasHeader(..))
+import Data.Set (Set, isSubsetOf, empty, insert)
+import Data.Time.Clock (NominalDiffTime)
+import GHC.Exts (toList)
+
+import Timetable hiding (timetables)
+import Scraping.GovData.TimeString (parseTimeStr)
+
+parseCsv :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> ByteString -> Either String [Timetable NominalDiffTime]
+parseCsv bs = toTimetables @e @ed <$> (parseWithHeader @e @ed bs <|> parse @e @ed bs)
+
+data Entry di da t r = Entry
+    { _direction :: di
+    , _days :: da
+    , _time :: t
+    , _remark :: r
+    }
+
+type NTDirection di = Newtype di Direction
+type NTDays da = Newtype da (Set Day)
+type NTTime t = Newtype t NominalDiffTime
+type NTRemark r = Newtype r ((Set Day, Set Modifier) -> (Set Day, Set Modifier))
+
+type NT di da t r ed =
+    ( NTDirection di
+    , NTDays da
+    , NTTime t
+    , NTRemark r
+    , EnumDays ed
+    , FromRecord (Entry di da t r)
+    , FromNamedRecord (Entry di da t r)
+    )
+
+class EnumDays a where
+    enumerated :: [a]
+    toDaySet :: a -> Set Day
+
+emptyRemark :: (NTDays da)=> Entry di da t r -> (Set Day, Set Modifier)
+emptyRemark (Entry _ d _ _) = (unpack d, empty)
+
+addModifier :: Modifier -> (Set Day, Set Modifier) -> (Set Day, Set Modifier)
+addModifier = second . insert
+
+modifyDays :: (Set Day -> Set Day) -> (Set Day, Set Modifier) -> (Set Day, Set Modifier)
+modifyDays = first
+
+toTimetables :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> [e] -> [Timetable NominalDiffTime]
+toTimetables entries = do
+    days <- enumerated
+    direction <- [FromPrimary, ToPrimary]
+    let ferries = toFerry @e @ed <$> findEntries direction days
+    [Timetable ferries (toDaySet days) direction]
+
+    where
+        remarkedEntries = patchDaySetByRemark @e @ed <$> entries
+
+        findEntries :: Direction -> ed -> [Entry di da t r]
+        findEntries direction days = filter (match direction days) remarkedEntries
+
+        match direction days (Entry direction' days' _ _) = direction == (unpack direction') && (matchDays days (unpack days'))
+
+        matchDays :: ed -> Set Day -> Bool
+        matchDays days days' = (toDaySet days) `isSubsetOf` days'
+
+
+toFerry :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> e -> Ferry NominalDiffTime
+toFerry e@(Entry _ _ t r) =
+    Ferry (unpack t) (snd $ (unpack r) $ emptyRemark e)
+
+patchDaySetByRemark :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> e -> Entry di da t r
+patchDaySetByRemark e@(Entry direction' _ time' r) = Entry direction' (pack $ fst $ (unpack r) $ emptyRemark e) time' r
+
+parseWithHeader :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> ByteString -> Either String [Entry di da t r]
+parseWithHeader bs = (toList . snd) <$> decodeByName bs
+
+parse :: forall e ed di da t r. (e ~ (Entry di da t r), NT di da t r ed)=> ByteString -> Either String [e]
+parse bs = toList <$> decode HasHeader bs
